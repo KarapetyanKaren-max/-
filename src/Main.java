990334791ecb0a9b84023c0dd901d3c237967257
Karen <karapetyankaren33933@gmail.com>
2024-11-03 12:02:08 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -52,13 +52,14 @@ public class Main {
 
         System.out.println();
         System.out.println("Задача 4");
+
         int[] code = {1, 2, 3};
-        for (int y = 0; y < code.length; y++) {
-            {
-                code[y] = code[y] + 1;
+        for (int y = 0; y < code.length; y++)
+            if (code[y] % 2 != 0) {
+                code[y]++;
             }
-        }
-        System.out.print(Arrays.toString(code));
-            }
-    }
+
+                    System.out.print(Arrays.toString(code));
+                }
+                }
 
