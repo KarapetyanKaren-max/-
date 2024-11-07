f201ed5b0d9d952df3642cd9712698684c7257a6
Karen <karapetyankaren33933@gmail.com>
2024-11-08 00:53:28 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int year = 2021;
+        if (year > 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год является высокостным ");
+        } else {
+            System.out.println(year + " год не является высокостным ");
+        }
     }
 }
