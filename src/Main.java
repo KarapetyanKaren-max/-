f16482a03995422286f71024893b61eb735d42b4
Karen <karapetyankaren33933@gmail.com>
2024-11-09 00:51:08 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
             System.out.println(year + " год является высокостным ");
         } else {
             System.out.println(year + " год не является высокостным ");
-
         }
     }
 }
