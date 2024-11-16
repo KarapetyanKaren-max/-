575c6e48694bf8c179c1335a4960f53a983f3384
Karen <karapetyankaren33933@gmail.com>
2024-11-17 00:48:03 +0400

    Initial commit

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,21 @@
-public class Author
-{
+public class Author {
+    private String nameAuthor;
+    private String surnameAuthor;
+
+    public Author(String nameAuthor, String surnameAuthor) {
+        this.nameAuthor = nameAuthor;
+        this.surnameAuthor = surnameAuthor;
+    }
+
+    public String getName() {
+        return nameAuthor;
+    }
+
+    public String getSurname() {
+        return surnameAuthor;
+    }
+
+    public String toString() {
+        return nameAuthor + " " + surnameAuthor;
+    }
 }
