bccb45ab3ad640c1dceebb27a563412324f1afdd
Karen <karapetyankaren33933@gmail.com>
2024-11-17 00:43:23 +0400

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+
+        private String booktitle;
+        private Author nameAuthor;
+        private int publicationYeae;
+
+        public Book(String booktitle, Author nameOfAuthor, int publicationYeae) {
+            this.booktitle = booktitle;
+            this.nameAuthor = nameOfAuthor;
+            this.publicationYeae = publicationYeae;
+        }
+
+        public String getBooktitle() {
+            return this.booktitle;
+        }
+
+        public Author getNameAuthor() {
+            return this.nameAuthor;
+        }
+
+        public int getPublicationYeae() {
+            return this.publicationYeae;
+        }
+
+        public void setPublicationYeae(int publicationYeae) {
+            this.publicationYeae = publicationYeae;
+        }
+    }
+}
