71ddd8d8192d43a92bc69a3b670cf5dfe50b136f
Karen <karapetyankaren33933@gmail.com>
2024-11-18 20:49:28 +0400

    Initial commit

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String nameAuthor;
     private String surnameAuthor;
@@ -20,8 +22,17 @@ public class Author {
     public String toString() {
         return nameAuthor + " " + surnameAuthor;
     }
-    public  boolean equals(Object o){
-        if (this -- o) return true;
-//        if (o -- null ||  )
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(nameAuthor, author.nameAuthor) && Objects.equals(surnameAuthor, author.surnameAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameAuthor, surnameAuthor);
     }
 }
