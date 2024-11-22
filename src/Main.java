84f54a576c0663372da8f53be99dba9cdddb4ed7
Karen <karapetyankaren33933@gmail.com>
2024-11-22 21:08:45 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public  static Employee createEmploye(String fullName, int department, int salary) {
+        Employee employee = new Employee(fullName, department, salary);
+        return employee;
     }
-}
+    public static int calculateSalary(Employee[] employees) {
+        int result = 0;
+        for (int i = 0; i < employees.length; i++) {
+            Employee employeeNew = employees[i];
+            if (employeeNew != null) {
+                result  = result + employeeNew.getSalary();
+
+            }
+        }
+        return result;
+    }
+    public static int calculateMaxSalary(Employee[] employees1) {
+        long maxSalary = - 1;
+        for (int i = 0; i < employees1.length; i++) {
+            //Employee employee = employees[i];
+            if (employees1 > maxSalary) {
+                maxSalary = employees1[i];
+            }
+        }
+    }
+    public static void main(String[] args){
+
+
+    }
+    }
