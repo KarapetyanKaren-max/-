1d02660d1aa7071dd86384cc727344ca5cda2a28
Karen <karapetyankaren33933@gmail.com>
2024-11-22 21:11:47 +0400

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class Employee {
+    String fullName;
+    int department;
+    int salary;
+    static int count;
+    private int id;
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this. salary = salary;
+        this.id = count++;
+    }
+    public static int id(int i) {
+        i++;
+        return i;
+    }
+    String getFullName() {
+        return this.fullName;
+    }
+    int getDepartment() {
+        return this.department;
+    }
+    int getSalary() {
+        return this.getSalary();
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary, id);
+    }
+
+    }
