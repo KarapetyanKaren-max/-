06dbbfacac3aa3d5d63988f657244ec74df2c838
Karen <karapetyankaren33933@gmail.com>
2024-12-27 18:24:53 +0400

    Initial commit

---
+++ src/org/skypro/Product.java
@@ -0,0 +1,28 @@
+package org.skypro;
+
+    public class Product {
+        public static void main(String[] args) {
+
+        }
+    private int name;
+    private int price;
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
+
+
+
